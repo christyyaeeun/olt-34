b7e762b7b8b11a410c6fdb7836ba0e174e0c8e39
Christy Park <christypark21@gmail.com>
2022-04-05 13:10:09 -0500

    adding yml config

--- src/components/AddPost.js
+++ src/components/AddPost.js
@@ -12,14 +12,25 @@
 import { Button } from '@chakra-ui/react'
 // import { DataStore } from 'aws-amplify';
 const AddPost = ({ id, title, content, image, postDate, handleDeletePost }) => {
-
+    async function onChange(e) {
+        const file = e.target.files[0];
+        try {
+          await Storage.put(file.name, file, {
+            contentType: "image/png", // contentType is optional
+          });
+        } catch (error) {
+          console.log("Error uploading file: ", error);
+        }
+      }
+      
+      <input type="file" onChange={onChange} />;
     
     return (
 		<div className='note'>
 			<span>{title}</span>
 			<div className='note-footer'>
 				<small>{content}</small>
-                {image}
+                {image}  
                 {postDate}
 				<Button	onClick={() => handleDeletePost(id)}
 					className='delete-icon'
